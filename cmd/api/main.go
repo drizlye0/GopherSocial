367dc5bb442f98949893600abd495f825ae27162
drizlye0 <danielkastillo03@gmail.com>
2025-05-06 00:21:16 -0500

    api implements store package

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/drizlye0/GopherSocial/internal/env"
+	"github.com/drizlye0/GopherSocial/internal/store"
 )
 
 func main() {
@@ -11,8 +12,11 @@ func main() {
 		addr: env.GetString("ADDR", ":8080"),
 	}
 
+	store := store.NewStorage(nil)
+
 	app := &application{
 		config: *cfg,
+		store:  store,
 	}
 
 	mux := app.mount()
