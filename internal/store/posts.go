33e159d8b1df88d6b00b806dd7d2cc495e221eac
drizlye0 <danielkastillo03@gmail.com>
2025-05-10 00:11:43 -0500

    fix: insert query on posts storage

--- internal/store/posts.go
+++ internal/store/posts.go
@@ -3,12 +3,14 @@ package store
 import (
 	"context"
 	"database/sql"
+
+	"github.com/lib/pq"
 )
 
 type Post struct {
 	ID        int64    `json:"id"`
-	Content   string   `json:"content"`
 	Title     string   `json:"title"`
+	Content   string   `json:"content"`
 	UserID    int64    `json:"user_id"`
 	Tags      []string `json:"tags"`
 	CreatedAt string   `json:"created_at"`
@@ -21,17 +23,17 @@ type PostStore struct {
 
 func (s *PostStore) Create(ctx context.Context, post *Post) error {
 	query := `
-	INSERTO INTO posts(content, title, user_id, tags) 
+	INSERT INTO posts(title, content, user_id, tags) 
 	VALUES($1, $2, $3, $4) RETURNING id, created_at, updated_at
 	`
 
 	err := s.db.QueryRowContext(
 		ctx,
 		query,
-		post.Content,
 		post.Title,
+		post.Content,
 		post.UserID,
-		post.Tags,
+		pq.Array(post.Tags),
 	).Scan(
 		&post.ID,
 		&post.CreatedAt,
