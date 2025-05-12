a6949a07601e1a238c6ff4e04f917612a33e06ce
drizlye0 <danielkastillo03@gmail.com>
2025-05-12 13:13:28 -0700

    feat: implements user comments in posts

---
+++ internal/store/comments.go
@@ -0,0 +1,52 @@
+package store
+
+import (
+	"context"
+	"database/sql"
+)
+
+type Comment struct {
+	ID         int64  `json:"id"`
+	UserID     int64  `json:"user_id"`
+	PostID     int64  `json:"post_id"`
+	Content    string `json:"content"`
+	Created_At string `json:"created_at"`
+	User       User   `json:"user"`
+}
+
+type CommentStore struct {
+	db *sql.DB
+}
+
+func (s *CommentStore) GetByPostId(ctx context.Context, postID int64) ([]Comment, error) {
+	query := `
+		SELECT c.id, c.user_id, c.post_id, c.content, c.created_at, users.username, users.id
+		FROM comments c
+		JOIN users ON users.id = c.user_id
+		WHERE c.post_id = $1
+		ORDER BY c.created_at DESC;
+	`
+
+	rows, err := s.db.QueryContext(ctx, query, postID)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var comments []Comment
+
+	for rows.Next() {
+		var c Comment
+		c.User = User{}
+
+		err := rows.Scan(&c.ID, &c.UserID, &c.PostID, &c.Content, &c.Created_At, &c.User.Username, &c.User.ID)
+		if err != nil {
+			return nil, err
+		}
+
+		comments = append(comments, c)
+	}
+
+	return comments, nil
+}
