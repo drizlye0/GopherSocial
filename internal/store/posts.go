3e2dd4ed189af988132a4cf0bcef156c0223ff6b
drizlye0 <danielkastillo03@gmail.com>
2025-05-15 13:05:06 -0700

    feat: optmistic concurrency control

--- internal/store/posts.go
+++ internal/store/posts.go
@@ -14,6 +14,7 @@ type Post struct {
 	Content   string    `json:"content"`
 	UserID    int64     `json:"user_id"`
 	Tags      []string  `json:"tags"`
+	Version   int       `json:"version"`
 	CreatedAt string    `json:"created_at"`
 	UpdatedAt string    `json:"updated_at"`
 	Comments  []Comment `json:"comments"`
@@ -51,7 +52,9 @@ func (s *PostStore) Create(ctx context.Context, post *Post) error {
 
 func (s *PostStore) GetByID(ctx context.Context, id int64) (*Post, error) {
 	query := `
-		SELECT id, title, content, user_id, tags, created_at, updated_at FROM posts WHERE id = $1
+		SELECT id, title, content, user_id, tags, created_at, updated_at, version
+		FROM posts 
+		WHERE id = $1
 	`
 	var post Post
 
@@ -67,6 +70,7 @@ func (s *PostStore) GetByID(ctx context.Context, id int64) (*Post, error) {
 		pq.Array(&post.Tags),
 		&post.CreatedAt,
 		&post.UpdatedAt,
+		&post.Version,
 	)
 
 	if err != nil {
@@ -109,13 +113,28 @@ func (s *PostStore) DeleteByID(ctx context.Context, postID int64) error {
 func (s *PostStore) UpdatePost(ctx context.Context, post *Post) error {
 	query := `
 		UPDATE posts
-		SET title = $1, content = $2
-		WHERE posts.id = $3
+		SET title = $1, content = $2, version = version + 1
+		WHERE posts.id = $3 AND version = $4
+		RETURNING version
 	`
 
-	_, err := s.db.ExecContext(ctx, query, post.Title, post.Content, post.ID)
+	err := s.db.QueryRowContext(
+		ctx,
+		query,
+		post.Title,
+		post.Content,
+		post.ID,
+		post.Version,
+	).Scan(
+		&post.Version,
+	)
 	if err != nil {
-		return err
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return ErrNoFound
+		default:
+			return err
+		}
 	}
 
 	return nil
