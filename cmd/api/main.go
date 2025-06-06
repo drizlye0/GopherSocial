19cbf8d799b016f6e1c02a28f0a17bde08fb1379
drizlye0 <danielkastillo03@gmail.com>
2025-06-05 21:55:11 -0500

    feat: implements initial docs with swagger

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -11,9 +11,26 @@ import (
 
 const version = "0.0.1"
 
+//	@title			GohperSocial API
+//	@description	This is a social network API for Gophers!
+//	@termsOfService	http://swagger.io/terms/
+
+//	@contact.name	API Support
+//	@contact.url	http://www.swagger.io/support
+//	@contact.email	support@swagger.io
+
+//	@license.name	Apache 2.0
+//	@license.url	http://www.apache.org/licenses/LICENSE-2.0.html
+
+//	@securityDefinitions.apikey	ApiKeyAuth
+//	@in							header
+//	@name						Authorization
+//	@description
+
 func main() {
 	cfg := &config{
-		addr: env.GetString("ADDR", ":8080"),
+		addr:   env.GetString("ADDR", ":8080"),
+		apiURL: env.GetString("EXTERNAL_URL", "localhost:3000"),
 		db: dbConfig{
 			addr:        env.GetString("DB_ADDR", "postgres://user:adminpass@localhost/social?sslmode=disabled"),
 			maxOpenConn: env.GetInt("DB_MAX_OPEN_CONN", 30),
