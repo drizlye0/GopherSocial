af8020de78af3eefaee13f0c4d4e1a4081b5db31
drizlye0 <danielkastillo03@gmail.com>
2025-06-13 23:03:02 -0500

    adds cors middleware

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -10,6 +10,7 @@ import (
 	"github.com/drizlye0/GopherSocial/internal/store"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	httpSwagger "github.com/swaggo/http-swagger/v2"
 	"go.uber.org/zap"
 )
@@ -49,6 +50,14 @@ type dbConfig struct {
 
 func (app *application) mount() http.Handler {
 	r := chi.NewMux()
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 
 	r.Use(middleware.RequestID)
 	r.Use(middleware.RealIP)
