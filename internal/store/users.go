6e66ca7ce2350c44624bcc5afb9aa1301b800ae0
drizlye0 <danielkastillo03@gmail.com>
2025-06-16 22:48:26 -0500

    add roles table, role_id column for users and fix db seed

--- internal/store/users.go
+++ internal/store/users.go
@@ -23,6 +23,7 @@ type User struct {
 	Password  password `json:"-"`
 	CreatedAt string   `json:"created_at"`
 	IsActive  bool     `json:"is_active"`
+	RoleID    int64    `json:"role_id"`
 }
 
 type password struct {
@@ -52,8 +53,8 @@ type UserStore struct {
 
 func (s *UserStore) Create(ctx context.Context, tx *sql.Tx, user *User) error {
 	query := `
-		INSERT INTO users(username, email, password)
-		VALUES ($1, $2, $3) RETURNING id, created_at
+		INSERT INTO users(username, email, password, role_id)
+		VALUES ($1, $2, $3, $4) RETURNING id, created_at
 	`
 
 	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
@@ -65,6 +66,7 @@ func (s *UserStore) Create(ctx context.Context, tx *sql.Tx, user *User) error {
 		user.Username,
 		user.Email,
 		user.Password.hash,
+		user.RoleID,
 	).Scan(
 		&user.ID,
 		&user.CreatedAt,
